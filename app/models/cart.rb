5180e0f3be97a160dc640059293f39bada99f349
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-22 14:01:29 +0900

    Add Cart & CartItem Relation

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,4 @@
 class Cart < ApplicationRecord
   belongs_to :user
+  acts_as_shopping_cart_using :cart_item
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,3 @@
 class CartItem < ApplicationRecord
+    acts_as_shopping_cart_item_for :cart
 end
